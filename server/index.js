7d64f7735ea00e81b77705ff4e637957ad40116a
kevinbece5 <kevinbece5@gmail.com>
2019-05-07 15:08:02 -0700

    starter file for server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`)
+})
