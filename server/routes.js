c0043b7dd01174b908755d487e5f131e8ec17737
kevinbece5 <kevinbece5@gmail.com>
2019-05-08 10:42:50 -0700

    defined CRUD routes for poem and Get for all poems

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const controller = require('./controllers');
 
-
+router.get('/poems', controller.poems.get);
+router.get('/poem/:id', controller.poem.get);
+router.post('/poem', controller.poem.post)
+router.delete('/poem/:id', controller.poem.delete)
+router.put('/poem/:id', controller.poem.update)
 
 module.exports = router;
