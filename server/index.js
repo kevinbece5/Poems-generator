31d32cb8a98dcd23646a9b8e5ac5c697e5c227df
kevinbece5 <kevinbece5@gmail.com>
2019-05-08 10:42:00 -0700

    connected routes to server as well as the db conn

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const routes = require('./routes');
+const db = require('./db');
 
+db.connect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.use(routes);
 app.listen(port, () => {
     console.log(`running on port ${port}`)
 })
