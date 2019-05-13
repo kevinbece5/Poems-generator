b48c60e2d4c65811f399d874c5736fa38e3a21e2
kevinbece5 <kevinbece5@gmail.com>
2019-05-12 22:25:47 -0700

    added timestamp to poem as well as styles

--- client/src/components/Poems.vue
+++ client/src/components/Poems.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <span v-if="poems.length" id="title">All Poems!</span>
+    <span v-else id="title">Oh no! You don't have any poems</span>
     <ul class="list-group list-group-flush">
-      <li :key="poem.id" v-for="poem in poems" class="list-group-item">
-        <span>{{poem.poem}}</span>
+      <li :key="poem.id" v-for="poem in poems" class="list-group-item listItem">
+        <span class="poem">{{poem.poem}}</span>
+        <span class="timeStamp">{{poem.timeStamp}}</span>
         <button v-on:click="deletePoem(poem.id)" type="button" class="btn btn-danger">Delete</button>
-        <button type="button" class="btn btn-info">Info</button>
       </li>
     </ul>
   </div>
@@ -38,4 +40,23 @@ export default {
 </script>
 
 <style scoped>
+#title {
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+  font-family: sans-serif;
+}
+.timeStamp {
+  width: 12%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.poem {
+  width: 80%;
+  display: flex;
+}
+.listItem {
+  display: flex;
+}
 </style>
